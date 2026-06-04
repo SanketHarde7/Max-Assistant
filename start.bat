cd2c66e767cf883996e8acb3453985debdbe7fe0
sanket <sanketharde62@gmail.com>
2026-06-04 17:36:39 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -13,5 +13,8 @@ pip install -r requirements.txt --quiet --disable-pip-version-check 2>nul
 echo [2/2] Handing over control to Rust Dictator...
 cd /d "%~dp0max-desktop"
 
+:: Add MinGW-w64 to PATH for Rust GNU toolchain (no Visual Studio Build Tools needed)
+set "PATH=%LOCALAPPDATA%\Microsoft\WinGet\Packages\BrechtSanders.WinLibs.POSIX.UCRT_Microsoft.Winget.Source_8wekyb3d8bbwe\mingw64\bin;%PATH%"
+
 :: Note: Ye 'dev' mode ka terminal open rahega jab tak hum final .exe nahi banate.
 npm run tauri dev
