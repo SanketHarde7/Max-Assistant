71bbfa470a5312354f1cd9b52728fd5d903222a8
sanket <sanketharde62@gmail.com>
2026-06-04 18:49:07 +0530

    update main.tsx

--- max-desktop/src/main.tsx
+++ max-desktop/src/main.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { getCurrentWindow } from "@tauri-apps/api/window";
 import App from "./App";
-import { ListeningOverlayFull } from "./ListeningOverlayFull.tsx"; // Naya component
+import { TextWindow } from "./TextWindow";
 
 const appWindow = getCurrentWindow();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    {/* Agar window ka naam 'overlay' hai toh premium border dikhao, warna normal Orb */}
-    {appWindow.label === "overlay" ? <ListeningOverlayFull /> : <App />}
+    {appWindow.label === "overlay" ? <TextWindow /> : <App />}
   </React.StrictMode>
 );
